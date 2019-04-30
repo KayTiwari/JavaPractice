37d175cfbbf851d8350ddb0d2e88586b3d78cb77
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-29 22:16:59 -0600

    Assignment + Inc/Dec operators

--- operators/operators.java
+++ operators/operators.java
@@ -2,8 +2,29 @@ public class operators {
     public static void main(String[] args){
         //arithmetic: +, -, *, /, %
         // a = 10; b = 10; c = a + b = 20;
-        int num1 = 100;
-        int num2 = 20;
-        System.out.println("Num1 + Num2 = " + (num1 + num2));
+        // int num1 = 100;
+        // int num2 = 20;
+        // System.out.println("Num1 + Num2 = " + (num1 + num2));
+
+        //Assigment operator
+        //Re-assigning values to variables
+        // int num1 = 10;
+        // int num2 = 20;
+        // num2 = num1;
+        // System.out.println("output = " +num2);
+
+
+
+        //Inc/Dec operator
+        //num++     auto-increment
+        //num--     auto-decrement
+        // int num1 = 100;
+        // int num2 = 200;
+        // num1++;
+        // num2--;
+        // System.out.println(num1);
+        // System.out.println(num2);
+
+
     }
 }
