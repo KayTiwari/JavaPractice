2c3a0fc15afd3001a30c44651d71c15ee13e6159
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-04-29 22:43:17 -0600

    elseif statements

---
+++ controlstatements/elseifstatements.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class elseifstatements{
+    public static void main(String[] args){
+        //if(statement){
+        // else if(statement)
+        // else (statement)
+        //}
+        // int age;
+        // Scanner inputDevice = new Scanner(System.in);
+        // System.out.println("Enter your age");
+        // age = inputDevice.nextInt();
+        // if(age)
+        //BORIGNGGNGNGGN
+    }
+}
