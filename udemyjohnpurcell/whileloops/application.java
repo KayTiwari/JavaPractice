d127eadf91bbb8d3c2bbfccb8fb252e23fb0776a
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-09 23:23:40 -0600

    while loops

---
+++ udemyjohnpurcell/whileloops/application.java
@@ -0,0 +1,12 @@
+public class application {
+    public static void main(String[] args){
+        int value = 0;
+        // boolean loop = value < 20;
+
+        while (value < 10){
+            System.out.println(true);
+            value++;
+        }
+        // System.out.println(loop);
+    }
+}
