3a70c9a93c57acba7dcd7e0ca5d723e11cbe321f
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-09 23:41:08 -0600

    while and dowhile loops

---
+++ udemytut/dowhileloop/dowhileloop.java
@@ -0,0 +1,18 @@
+public class dowhileloop{
+    public static void main(String[] args){
+        int i = 10;
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i > 1);
+    }
+}
+
+// do {
+//     function logic
+// }
+
+// while (boolean){
+    //when boolean is true, the function goes back to the 'do'
+// }
--- udemytut/whileloop/whileloop.java
+++ udemytut/whileloop/whileloop.java
@@ -1,5 +1,12 @@
 public class whileloop{
     public static void main(String[] args){
-        
+        int i = 0;
+
+        while (i > 1){
+            System.out.println(i);
+            i++;
+        }
     }
 }
+
+//while boolean = true => do this
