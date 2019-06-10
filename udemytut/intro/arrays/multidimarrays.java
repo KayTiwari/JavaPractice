8bf5b79dd9fb7f49be01cd7de9cab73937f0a6fc
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-09 23:49:54 -0600

    single and multi dimensional arrays

---
+++ udemytut/intro/arrays/multidimarrays.java
@@ -0,0 +1,10 @@
+public class multidimarrays{
+    public static void main(String[] args){
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5 ,6, 8},
+            {7}
+        };
+        System.out.println(a[1].length);
+    }
+}
